efb66ae6a33b639457f5e96be1ae504858b9a6d3
zhouyong <zhouyong03@meituan.com>
2019-06-17 18:10:16 +0800

    test science numeric string -> double

--- base/test/type_conversion_test.cc
+++ base/test/type_conversion_test.cc
@@ -1,8 +1,14 @@
 #include "type_conversion.h"
 
 int main(int argc, char ** argv) {
-  const char* xx = "123.456";
-  float v = openmi::tools::StringToNum<float>(std::string(xx));
-  printf("v: %f\n",v);
+  //const char* xx = "123.456";
+  //const char* xx = "1.60114092E8";
+  const char* xx = "160114099";
+  float v = openmi::StringToNum<float>(std::string(xx));
+  printf("float v: %f\n",v);
+  double dv = openmi::StringToNum<double>(std::string(xx));
+  printf("double v: %f\n", dv);
+  long lv = (long) dv;
+  printf("int v: %ld\n", lv);
   return 0;  
 }
